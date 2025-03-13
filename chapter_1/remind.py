18bf5e25c8b33bbc715e40695a53ca15ab19b6de
JIhub-gif <bard4560@naver.com>
2025-03-13 13:57:28 +0900

    remind문제풀이

---
+++ chapter_1/remind.py
@@ -0,0 +1,18 @@
+a=80
+b=75
+c=55
+print((a+b+c)/3)
+
+pin = ("881120-1068234")
+print(pin[0:6])
+print(pin[7:])
+print(pin[7])
+
+d=[1,3,5,4,2]
+d.reverse()
+d.sort()
+print(d)
+
+e=1,2,3
+e=e+(4,)
+print(e)
